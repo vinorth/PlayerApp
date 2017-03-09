c842e76accc414a58a97df3bf7b26b6445964f0c
vinorth <vinorth_v@hotmail.com>
2017-03-08 23:57:57 -0500

    Added player model, players controller and corresponding views.

---
+++ PlayerApp/PlayerApp/Controllers/PlayersController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PlayerApp.Models;
+
+namespace PlayerApp.Controllers
+{
+    public class PlayersController : Controller
+    {
+        private PlayerAppContext db = new PlayerAppContext();
+
+        // GET: Players
+        public ActionResult Index()
+        {
+            return View(db.PlayerModels.ToList());
+        }
+
+        // GET: Players/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PlayerModel playerModel = db.PlayerModels.Find(id);
+            if (playerModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(playerModel);
+        }
+
+        // GET: Players/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Players/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Rating")] PlayerModel playerModel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.PlayerModels.Add(playerModel);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(playerModel);
+        }
+
+        // GET: Players/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PlayerModel playerModel = db.PlayerModels.Find(id);
+            if (playerModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(playerModel);
+        }
+
+        // POST: Players/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Rating")] PlayerModel playerModel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(playerModel).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(playerModel);
+        }
+
+        // GET: Players/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PlayerModel playerModel = db.PlayerModels.Find(id);
+            if (playerModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(playerModel);
+        }
+
+        // POST: Players/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            PlayerModel playerModel = db.PlayerModels.Find(id);
+            db.PlayerModels.Remove(playerModel);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ PlayerApp/PlayerApp/Models/PlayerModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PlayerApp.Models
+{
+    public class PlayerModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Rating { get; set; }
+    }
+}
