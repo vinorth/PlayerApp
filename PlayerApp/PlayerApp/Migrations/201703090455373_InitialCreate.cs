1c32115b7c9244330b0c554c51278c95c08ed3c8
vinorth <vinorth_v@hotmail.com>
2017-03-10 18:27:22 -0500

    Customized p and h tags and enabled code first migrations.

---
+++ PlayerApp/PlayerApp/Migrations/201703090455373_InitialCreate.cs
@@ -0,0 +1,27 @@
+namespace PlayerApp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.PlayerModels",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Rating = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.PlayerModels");
+        }
+    }
+}
---
+++ PlayerApp/PlayerApp/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace PlayerApp.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<PlayerApp.Models.PlayerAppContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "PlayerApp.Models.PlayerAppContext";
+        }
+
+        protected override void Seed(PlayerApp.Models.PlayerAppContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
